59851432c7503302625801ede296926d7c3a3382
Krystan Menne <krystan@yup.local>
2016-01-22 14:22:35 -0800

    add path to update stylist information

--- app.rb
+++ app.rb
@@ -37,6 +37,53 @@ post('/stylists') do
   redirect('/stylists')
 end
 
+get('/stylists/:id') do
+  @id = params[:id].to_i()
+  @stylist = Stylist.find(@id)
+  @clients = @stylist.clients()
+  erb(:stylist)
+end
+
+patch('/stylists/:id') do
+  id = params[:id].to_i()
+  stylist = Stylist.find(id)
+
+  first_name = params[:first_name]
+  last_name = params[:last_name]
+
+  if params[:phone].length() > 0
+    phone = params[:phone]
+  else
+    phone = 'NEED PHONE NUMBER'
+  end
+
+  unless first_name == stylist.first_name()
+    stylist.update({
+      :first_name => first_name
+      })
+  end
+
+  unless last_name == stylist.last_name()
+    stylist.update({
+      :last_name => last_name
+      })
+  end
+
+  unless phone == stylist.phone()
+    stylist.update({
+      :phone => phone
+      })
+  end
+
+  redirect("/stylists/#{id}")
+end
+
+get('/stylists/:id/edit') do
+  @id = params[:id].to_i()
+  @stylist = Stylist.find(@id)
+  erb(:stylist_update)
+end
+
 get('/clients') do
   @clients = Client.sort_by('first_name', 'ASC')
   @stylists = Stylist.sort_by('first_name', 'ASC')
--- spec/integration_spec.rb
+++ spec/integration_spec.rb
@@ -8,6 +8,7 @@ set(:show_exceptions, false)
 describe('adding a stylist', {:type => :feature}) do
   it('allows the user to add a new stylist to the database') do
     visit('/')
+    click_link('Stylists')
     fill_in('first_name', :with => 'Julia')
     fill_in('last_name', :with => 'Stiles')
     fill_in('phone', :with => '503-555-4242')
@@ -15,6 +16,23 @@ describe('adding a stylist', {:type => :feature}) do
     expect(page).to(have_content('Julia'))
     expect(page).to(have_content('Stiles'))
     expect(page).to(have_content('503-555-4242'))
+  end
+end
+
+describe('updating a stylist', {:type => :feature}) do
+  it('allows the user to update a stylist database entry') do
+    test_stylst = create_test_stylist()
+    test_stylst.save()
+    test_stylist_id = test_stylst.id().to_s()
+    visit('/stylists')
+    click_link("update_#{test_stylist_id}")
+    fill_in('first_name', :with => 'Jamie')
+    fill_in('last_name', :with => 'Styles')
+    fill_in('phone', :with => '503-555-4243')
+    click_on('Update')
+    expect(page).to(have_content('Jamie'))
+    expect(page).to(have_content('Styles'))
+    expect(page).to(have_content('503-555-4243'))
   end
 end
 
